810d89f0707bfaa6a600e1cec2ab67f6bdfd8def
DangLe1996 <baodang153@gmail.com>
2021-03-31 23:47:28 -0400

    plot full usa map

--- Source/Hierachycal.py
+++ Source/Hierachycal.py
@@ -1,42 +1,31 @@
-from urllib.request import urlopen
+
+
+import pandas as pd
+
+liveData = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv",  dtype={"fips": str})
 import json
-from sklearn.cluster import AgglomerativeClustering
+from urllib.request import urlopen
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
-import pandas as pd
-data = pd.read_csv('data/covid_confirmed_usafacts.csv')
+#%%
 
-df = pd.DataFrame(data, columns=['2020-03-23', '2021-03-23'])
-df1 = data.iloc[:, 4:]
-
+from sklearn.cluster import AgglomerativeClustering
 km = AgglomerativeClustering(n_clusters=10 )
-km.fit(df1)
-km.labels_
-data['classification'] = km.labels_.tolist()
-
-new_df = data[['countyFIPS', 'classification']]
-
+test = liveData[['county','cases']]
+#%%
+km.fit(liveData[['cases']])
+liveData['classification'] = km.labels_
+#%%
 
 import plotly.graph_objects as go
-
-fig = go.Figure(go.Choroplethmapbox(geojson=counties,  locations=new_df['countyFIPS'],
-                                    z=new_df['classification'],
-                                    colorscale="Viridis", zmin=0, zmax=12,
+fig = go.Figure(go.Choroplethmapbox(geojson=counties, locations=liveData.fips, z=liveData.classification,
+                                    colorscale="Viridis", zmin=0, zmax=10,
                                     marker_opacity=0.5, marker_line_width=0))
 fig.update_layout(mapbox_style="carto-positron",
                   mapbox_zoom=3, mapbox_center = {"lat": 37.0902, "lon": -95.7129})
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+fig.show(renderer="browser")
+#%%
 
 
 
-import plotly.express as px
-
-fig = px.choropleth(new_df, geojson=counties, locations='countyFIPS', color='classification',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 10),
-                           scope="usa",
-                           labels={'classification':'cluster'}
-                          )
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
